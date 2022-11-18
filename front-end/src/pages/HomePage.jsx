0a312825f9d2dc3e0e0a27e573aa158d317c931c
Acyd <briandika.mankin@gmail.com>
2022-11-18 15:08:10 +0700

    GRAM-5-PAGINATION (#5)

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -6,6 +6,7 @@ import Axios from "axios";
 import Card from "react-bootstrap/Card";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import Button from "react-bootstrap/Button";
 
 // CSS
 import "../css/style.css";
@@ -13,36 +14,82 @@ import "../css/style.css";
 export const HomePage = () => {
   const [books, setBooks] = useState([]);
   const [category, setCategory] = useState();
+  const [sort, setSort] = useState();
+  const [direction, setDirection] = useState();
+  const [pagination, setPagination] = useState(0);
+  const [page, setPage] = useState(1);
+  const [num, setNum] = useState(0);
 
   const url = "http://localhost:2000/book/";
 
   const getBooks = async () => {
     try {
-      const newURL = category ? url + `filter?data=${category}` : `${url}all`;
-      const res = await Axios.get(newURL);
+      const newURL = category
+        ? url + `filter?genre=${category}&`
+        : `${url}all?`;
+      const sortURL = sort ? newURL + `sort=${sort}&` : newURL;
+
+      // if (num === 0) {
+      //   setDirection();
+      // } else if (num === 1) {
+      //   setDirection("ASC");
+      // } else if (num === 2) {
+      //   setDirection("DESC");
+      // }
+
+      const directionURL = direction
+        ? sortURL + `direction=${direction}&`
+        : sortURL;
+
+      const paginationURL = directionURL + `pagination=${pagination}`;
+
+      const res = await Axios.get(paginationURL);
+      console.log(num);
       setBooks(res.data);
     } catch (err) {
       console.log(err);
     }
   };
 
+  // const pageCounter = () => {
+  //   if (page > 0) {
+  //     setPage(page + 1)
+  //   }
+  // }
+
   useEffect(() => {
     getBooks();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [category]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [category, sort, direction, pagination, num]);
+
+  // const counter = () => {
+  //   num++;
+  //   if (num > 2) {
+  //     num = 0;
+  //   }
+  // };
 
   return (
     <div className="container">
       <DropdownButton id="dropdown-item-button" title="Filter">
-        <Dropdown.Item as="button" onClick={() => {
-          setCategory();
-        }}>
+        <Dropdown.Item
+          as="button"
+          onClick={() => {
+            setCategory();
+            setSort();
+            setDirection();
+          }}
+        >
           All
         </Dropdown.Item>
         <Dropdown.Item
           as="button"
           onClick={() => {
             setCategory("romance");
+            setSort();
+            setDirection();
+            setPagination(0);
+            setPage(1);
           }}
         >
           Romance
@@ -50,10 +97,47 @@ export const HomePage = () => {
         <Dropdown.Item
           as="button"
           onClick={() => {
-            setCategory("action");
+            setCategory("horror");
+            setSort();
+            setDirection();
+            setPagination(0);
+            setPage(1);
           }}
         >
-          Action
+          Horror
+        </Dropdown.Item>
+      </DropdownButton>
+
+      <DropdownButton id="dropdown-item-button" title="Sort">
+        <Dropdown.Item
+          as="button"
+          onClick={() => {
+            setSort("title");
+            setDirection("ASC");
+            setPagination(0);
+            setPage(1);
+          }}
+        >
+          Title ASC
+        </Dropdown.Item>
+        <Dropdown.Item
+          as="button"
+          onClick={() => {
+            setSort("title");
+            setDirection("DESC");
+            setPagination(0);
+            setPage(1);
+          }}
+        >
+          Title DESC
+        </Dropdown.Item>
+        <Dropdown.Item
+          as="button"
+          onClick={() => {
+            num >= 2 ? setNum(0) : setNum(num + 1);
+          }}
+        >
+          Title
         </Dropdown.Item>
       </DropdownButton>
 
@@ -71,6 +155,42 @@ export const HomePage = () => {
             </Card>
           );
         })}
+      </div>
+
+      <div className="containerButton">
+        {books.length === 8 ? (
+          <Button
+            variant="primary"
+            onClick={() => {
+              setPagination(pagination + 8);
+              setPage(page + 1);
+            }}
+          >
+            Next
+          </Button>
+        ) : (
+          <Button variant="primary" disabled>
+            Next
+          </Button>
+        )}
+
+        <h3>Page {page}</h3>
+
+        {page !== 1 ? (
+          <Button
+            variant="primary"
+            onClick={() => {
+              setPagination(pagination - 8);
+              setPage(page - 1);
+            }}
+          >
+            Previous
+          </Button>
+        ) : (
+          <Button variant="primary" disabled>
+            Previous
+          </Button>
+        )}
       </div>
     </div>
   );
