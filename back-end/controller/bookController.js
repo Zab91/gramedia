44c7739179898c15e179319ae431a2b979bf9898
Zabal Ramadhan <114215191+Zab91@users.noreply.github.com>
2022-11-24 10:45:18 +0700

    GRAM-19 Revisi

--- back-end/controller/bookController.js
+++ back-end/controller/bookController.js
@@ -3,6 +3,25 @@ const db = require("../models");
 const book = db.Book;
 
 module.exports = {
+  add: async (req, res) => {
+    try {
+      const { title, author, publisher, category, year } = req.body;
+      //   console.log(bookName);
+      //   console.log(req.body);
+
+      await book.create({
+        title,
+        author,
+        publisher,
+        category,
+        year,
+      });
+
+      res.status(200).send("Book Added");
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  },
   all: async (req, res) => {
     try {
       const { sort, direction, pagination } = req.query;
@@ -52,7 +71,7 @@ module.exports = {
 
       const result = await book.findAll({
         where: {
-          Genre: genre,
+          category: genre,
         },
         order: [[sort ? sort : "id", direction ? direction : "ASC"]],
         limit: 10,
